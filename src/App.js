ac822bcf10b9678b9cc0bfcb48fe8ab33af39b49
Edcarl Adraincem <iamedcarl@users.noreply.github.com>
2019-07-15 12:33:55 -0700

    Add functionality for creating a new todo

--- src/App.js
+++ src/App.js
@@ -5,29 +5,49 @@ function App() {
   const [todos, setTodos] = useState([
     {
       content: 'Learn React hooks',
-      isCompleted: false,
+      isCompleted: false
     },
     {
       content: 'Dinner with wife',
-      isCompleted: false,
+      isCompleted: false
     },
     {
       content: 'Pick up some milk',
-      isCompleted: false,
-    },
+      isCompleted: false
+    }
   ]);
+
+  const handleKeyDown = (e, index) => {
+    if (e.key === 'Enter') {
+      createTodoAtIndex(e, index);
+    }
+  };
+
+  const createTodoAtIndex = (e, index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index + 1, 0, {
+      content: '',
+      isCompleted: false,
+    });
+    setTodos(newTodos);
+    setTimeout(() => {
+      document.forms[0].elements[index+1].focus();
+    }, 0);
+  };
 
   return (
     <div className="app">
-      <header className="app-header">
-        React Todos
-      </header>
+      <header className="app-header">React Todos</header>
       <form className="todo-list">
         <ul>
           {todos.map((todo, index) => (
             <div className="todo">
               <div className="checkbox" />
-              <input type="text" value={todo.content} />
+              <input
+                type="text"
+                value={todo.content}
+                onKeyDown={e => handleKeyDown(e, index)}
+              />
             </div>
           ))}
         </ul>
